cef75c52ff9ed241b0756792a2b5f77e6581ef58
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2025-02-18 12:09:43 +0100

    variable Types

--- main.ts
+++ main.ts
@@ -1,2 +1,64 @@
+export {}
 let message = 'Welcome back!';
 console.log(message);
+
+let x = 10
+const y = 20
+
+let sum;
+const title = "Codevolution";
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let Name: string = 'Daniel';
+
+let sentece: string = `My name is ${Name} 
+I am a beginner in Typescript`
+
+console.log(sentece);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+let person1: [string, number] = ["Daniel",19];
+
+enum Color {Red = 5 , Green, Blue};
+
+let c: Color = Color.Green;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = true
+randomValue = 'Daniel';
+
+let myVariable: unknown = 'Daniel';
+
+function hasName(obj: any): obj is {name: string}{
+    return !!obj && 
+    typeof obj === "object" && 
+    "name" in obj
+}
+
+if (hasName(myVariable)){
+    console.log(myVariable.name);
+}
+(myVariable as string).toUpperCase();
+
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+let anyType: any;
+anyType = 20;
+anyType = true;
+
+ 
